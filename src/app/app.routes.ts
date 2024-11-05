6a54a1ee94f72a4982f0f0070a7e11c54dac88d4
lovekeshpal <lovekeshfgr@gmail.com>
2024-11-05 22:58:43 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AdminLoginComponent } from './components/admin/admin-login/admin-login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'admin-login', component: AdminLoginComponent}
+];
