c03bce3620b33cb9178fc261a0ec3160659232ed
lovekeshpal <lovekeshfgr@gmail.com>
2024-11-06 22:20:26 +0530

    default routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { AdminLoginComponent } from './components/admin/admin-login/admin-login.component';
 
 export const routes: Routes = [
+    {path: '', redirectTo: 'admin-login', pathMatch: 'full'},
     {path: 'admin-login', component: AdminLoginComponent}
 ];
