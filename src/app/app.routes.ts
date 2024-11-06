ae3217b81806a9c06759e3ccd7e96f18308423e7
lovekeshpal <lovekeshfgr@gmail.com>
2024-11-06 23:32:48 +0530

    homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { AdminLoginComponent } from './components/admin/admin-login/admin-login.component';
 import { AdminRegisterComponent } from './components/admin/admin-register/admin-register.component';
+import { UserLoginComponent } from './components/user/user-login/user-login.component';
+import { UserRegisterComponent } from './components/user/user-register/user-register.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'admin-login', pathMatch: 'full'},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    // home
+    { path: 'home', component: HomeComponent },
+    // admin routes
     { path: 'admin-login', component: AdminLoginComponent },
     { path: 'admin-register', component: AdminRegisterComponent },
+    // user routes
+    { path: 'user-login', component: UserLoginComponent },
+    { path: 'user-register', component: UserRegisterComponent },
 ];
