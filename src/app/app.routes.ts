286adcdec0dd6c32a92e4918a242edb388c36013
Yasowant <yasowant1998@gmail.com>
2024-11-06 22:16:42 +0530

    updated code for admin ui-change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AdminLoginComponent } from './components/admin/admin-login/admin-login.component';
+import { AdminRegisterComponent } from './components/admin/admin-register/admin-register.component';
 
 export const routes: Routes = [
-    {path: 'admin-login', component: AdminLoginComponent}
+  { path: 'admin-login', component: AdminLoginComponent },
+  { path: 'admin-register', component: AdminRegisterComponent },
 ];
