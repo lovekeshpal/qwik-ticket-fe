2bd3c01395e9697833054f2dd553404c11ee7371
Yasowant <yasowant1998@gmail.com>
2024-11-07 23:19:56 +0530

    updated code for admin ui-change

--- src/app/components/admin/admin-register/admin-register.component.ts
+++ src/app/components/admin/admin-register/admin-register.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-admin-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './admin-register.component.html',
-  styleUrl: './admin-register.component.scss'
+  styleUrl: './admin-register.component.scss',
 })
 export class AdminRegisterComponent {
+  adminRegisterForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.adminRegisterForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.adminRegisterForm.valid) {
+      console.log(this.adminRegisterForm.value);
+      // this.adminLoginForm.reset();
+      this.adminRegisterForm.get('password')?.setValue('');
+      // API call
+    } else {
+      console.log('Form is not valid');
+    }
+  }
 }
