2ae148a6c8a4803aa1891b658e5912a48ddb9163
lovekeshpal <lovekeshfgr@gmail.com>
2024-11-07 22:52:22 +0530

    admin register form

--- src/app/components/admin/admin-login/admin-login.component.ts
+++ src/app/components/admin/admin-login/admin-login.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './admin-login.component.html',
   styleUrl: './admin-login.component.scss',
 })
-export class AdminLoginComponent {}
+export class AdminLoginComponent {
+  adminLoginForm!: FormGroup; 
+
+  constructor(private formBuilder: FormBuilder){
+    this.adminLoginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]], 
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if(this.adminLoginForm.valid){
+      console.log(this.adminLoginForm.value);
+      // this.adminLoginForm.reset(); 
+      this.adminLoginForm.get('password')?.setValue('')
+      // API call 
+    } else {
+      console.log('Form is not valid')
+    }
+  }
+}
