6ecbdfb93d23da89a732d1e089061468238b3d56
Yasowant <yasowant1998@gmail.com>
2024-11-21 22:57:36 +0530

    http request

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
+//https://fakestoreapi.com/products
 export class HomeComponent {
+  constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    this.fetchProducts();
+  }
+
+  fetchProducts(): void {
+    this.http.get('https://fakestoreapi.com/products').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error(err) {
+        console.error('Error: ' + err);
+      },
+      complete() {
+        console.log('Completed');
+      },
+    });
+  }
 }
