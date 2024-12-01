865188506d732a6e650722b15f3f0aeb8728d7f7
lovekeshpal <lovekeshfgr@gmail.com>
2024-12-01 23:29:58 +0530

    formArray

--- src/app/components/admin/create-event/create-event.component.ts
+++ src/app/components/admin/create-event/create-event.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {
   FormBuilder,
@@ -5,12 +6,13 @@ import {
   ReactiveFormsModule,
   FormArray,
   Validators,
+  FormControl,
 } from '@angular/forms';
 
 @Component({
   selector: 'app-create-event',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './create-event.component.html',
   styleUrl: './create-event.component.scss',
 })
@@ -22,11 +24,19 @@ export class CreateEventComponent {
       eventName: ['', Validators.required],
       isPrivate: [false],
       heroImage: [null],
-      venues: this.formBuilder.array([]),
+      venues: this.formBuilder.array([
+        new FormControl('', Validators.required),
+      ]),
       startTime: ['', Validators.required],
       endTime: ['', Validators.required],
       hasSegments: [false],
-      segments: this.formBuilder.array([]),
+      segments: this.formBuilder.array([
+        this.formBuilder.group({
+          segmentName: new FormControl('', Validators.required),
+          startDateTime: new FormControl('', Validators.required),
+          endDateTime: new FormControl('', Validators.required),
+        }),
+      ]),
       ticketsAvailable: [0, [Validators.required, Validators.min(1)]],
       ticketPrice: [0, [Validators.required, Validators.min(0)]],
       isParkingAvailable: [false],
@@ -42,5 +52,18 @@ export class CreateEventComponent {
     } else {
       console.log('Form is invalid');
     }
+  }
+
+  get venues(): FormArray {
+    return this.eventForm.get('venues') as FormArray;
+  }
+
+  addVenue(): void {
+    const venueControl = new FormControl('', Validators.required);
+    this.venues.push(venueControl);
+  }
+
+  removeVenue(index: number): void {
+    this.venues.removeAt(index);
   }
 }
